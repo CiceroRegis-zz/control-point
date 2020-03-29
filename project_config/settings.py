2acdbc22fd9d37a115d10abf39987b53c8247443
cicero_regis <cicero.regis95@gmail.com>
2020-03-29 11:06:20 -0300

    Update settings.py

--- project_config/settings.py
+++ project_config/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'cicerooliveira091@gmail.com'
-EMAIL_HOST_PASSWORD = 'Meups32015'
+EMAIL_HOST_USER = '******@gmail.com'
+EMAIL_HOST_PASSWORD = '******'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
